f7c0a09facf4ad07ce28aafedb5e154f2a0016cd
Jordan Frey <36864344+FreyGeospatial@users.noreply.github.com>
2024-10-25 06:24:25 -0400

    an ok option for squash and merges for linting shell script (#11)

--- .github/scripts/modified_files.sh
+++ .github/scripts/modified_files.sh
@@ -1,3 +1,3 @@
 # echos modified python files, but excludes those deleted
-changed_files=$(git diff --diff-filter=d --name-only $(git merge-base HEAD remotes/origin/main) HEAD | grep .py)
+changed_files=$(git diff --diff-filter=d --name-only $(git merge-base HEAD remotes/origin/main) HEAD${for_merge_trigger} | grep .py)
 echo ${changed_files}
